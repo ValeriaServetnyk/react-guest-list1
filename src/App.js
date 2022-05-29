26ccce8325b2cee5d7cd7cd81ca57f76a8ed8b65
ValeriaServetnyk <102434294+ValeriaServetnyk@users.noreply.github.com>
2022-05-29 21:25:17 +0200

    Add changes and corrected mistakes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 export default function App() {
-
   // get this from the user
 
   const [firstName, setFirstName] = useState('');
@@ -12,26 +11,25 @@ export default function App() {
 
   const baseUrl = 'https://guest-list-database.herokuapp.com';
 
-// get this from API
+  // get this from API
 
   const [guests, setGuests] = useState([]);
   const [loading, setLoading] = useState(true);
   const [stateUpdate, setStateUpdate] = useState(true);
 
+  // establish connection with the database
 
-// establish connection with the database
-
-useEffect(() => {
-async function getGuests () {
-  const response = await fetch(`${baseUrl}/guests`);
-  const allGuests = await response.json();
-setLoading(false);
-setGuests(allGuests);
-console.log('fetch done');
-};
-getGuests().catch(() =>{
-  console.log('fetch failed');
-});
+  useEffect(() => {
+    async function getGuests() {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      setLoading(false);
+      setGuests(allGuests);
+      console.log('fetch done');
+    }
+    getGuests().catch(() => {
+      console.log('fetch failed');
+    });
   }, [stateUpdate]);
 
   // send new guest to the database
@@ -40,129 +38,135 @@ getGuests().catch(() =>{
     const response = await fetch(`${baseUrl}/guests`, {
       method: 'POST',
       headers: {
-        'Content-Type':
-        'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         firstName: firstName,
         lastName: lastName,
       }),
     });
-const addNewGuest = await response.json();
-guests.push(addNewGuest);
-setFirstName('');
-setLastName('');
+    const addNewGuest = await response.json();
+    guests.push(addNewGuest);
+    setFirstName('');
+    setLastName('');
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     createGuest().catch(() => {
-      console.log ('guest not created');
+      console.log('guest not created');
     });
   };
 
-// remove guest
+  // remove guest
 
-async function deleteGuest(id) {
-const response = await fetch(`${baseUrl}/guests/${id}`,
-{ method: 'DELETE',
-});
-await response.json();
-setStateUpdate(!stateUpdate);
-}
+  async function deleteGuest(id) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'DELETE',
+    });
+    await response.json();
+    setStateUpdate(!stateUpdate);
+  }
 
-// update attendance via checkbox
+  // update attendance via checkbox
 
-
-  async function updateAttendance( id, attendance) {
+  async function updateAttendance(id, attendance) {
     const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ attending: attendance }),
-    }
-    );
+    });
     await response.json();
     setStateUpdate(!stateUpdate);
   }
 
-  if(loading) {
-    return <h1>Loading...</h1>
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
 
-return (
-  <div className='Main'>
-    <div className='textContainer'>
-      <h1>My  celebration of the first job offer</h1>
-      <p>After successfully finishing the Upleveled bootcamp I got my first amazing job offer and since now I have money I can throw a massive party.</p>
-      <p>Add your name below to make sure you are on the list!</p>
-    </div>
-    <form className='dataInput' onSubmit={handleSubmit}>
-    <label className='Fields'>
+  return (
+    <div className="Main">
+      <div className="textContainer">
+        <h1>My celebration of the first job offer</h1>
+        <p>
+          After successfully finishing the Upleveled bootcamp I got my first
+          amazing job offer and since now I have money I can throw a massive
+          party.
+        </p>
+        <p>Add your name below to make sure you are on the list!</p>
+      </div>
+      <form className="dataInput" onSubmit={handleSubmit}>
+        <label className="Fields">
           First Name:
-         <input
-           name="firstName"
+          <input
+            name="firstName"
             placeholder="Add your name"
             value={firstName}
-            disabled={loading ? true : false}
             onChange={(event) => {
               setFirstName(event.currentTarget.value);
             }}
           />
-          </label>
-          <label className='Fields'>
- Last Name:
-<input
-name="lastName"
-placeholder="Add your last name"
-value={lastName}
-disabled={loading ? true : false}
- onChange={(event) => {
- setLastName(event.currentTarget.value);
-}}
-onKeyDown={(event) => {
-if( event.key === 'Enter')
-{event.preventDefault();
-createGuest ([firstName, lastName]).catch(() => {console.log('guest not added');
-});
-}
-}}
-/>
-</label>
-    </form>
-    {guests.map((guest) => {
-      return (
-      <div key={guest.id} data-test-id="guest">
-        <hr />
-        <div className='generatedList'>
-          <p className='listItems'>
-            {guest.firstName}
-            {' '}
-            {guest.lastName}</p>
-          <label>
-            <input className='Checkbox'
-            aria-label="attending"
-            type="checkbox"
-            checked={guest.attending}
-            onChange={() => {updateAttendance(guest.id, !guest.attending).catch(() => { console.log('guest not updated');
-          })
-          console.log(guest.attending);
-        }}
-        />
-        {guest.attending ? ' attending' : ' not attending'}
         </label>
-<button aria-label="Remove"
-onClick={() => {deleteGuest(guest.id).catch(() => {console.log('guest not deleted');
-})
-}}>
-  {' '}
-  Remove
-</button>
-        </div>
-      </div>
-      )
-    })}
-  </div>
- )
- }
+        <label className="Fields">
+          Last Name:
+          <input
+            name="lastName"
+            placeholder="Add your last name"
+            value={lastName}
+            onChange={(event) => {
+              setLastName(event.currentTarget.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                event.preventDefault();
+                createGuest([firstName, lastName]).catch(() => {
+                  console.log('guest not added');
+                });
+              }
+            }}
+          />
+        </label>
+      </form>
+      {guests.map((guest) => {
+        return (
+          <div key={guest.id} data-test-id="guest">
+            <hr />
+            <div className="generatedList">
+              <p className="listItems">
+                {guest.firstName} {guest.lastName}
+              </p>
+              <label>
+                <input
+                  className="Checkbox"
+                  aria-label="attending"
+                  type="checkbox"
+                  checked={guest.attending}
+                  onChange={() => {
+                    updateAttendance(guest.id, !guest.attending).catch(() => {
+                      console.log('guest not updated');
+                    });
+                    console.log(guest.attending);
+                  }}
+                />
+                {guest.attending ? ' attending' : ' not attending'}
+              </label>
+              <button
+                aria-label="Remove"
+                onClick={() => {
+                  deleteGuest(guest.id).catch(() => {
+                    console.log('guest not deleted');
+                  });
+                }}
+              >
+                {' '}
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
